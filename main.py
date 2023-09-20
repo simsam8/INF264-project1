8845611bae57255b2b72e8069b0249aa40e6ce1a
simsam8 <simvedaa@gmail.com>
2023-09-20 11:44:15 +0200

    time calculation

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from Evaluation import Evaluation
+from time import time
 
-seed = None
+seed = 15
+
+print(f"Run using seed: {seed}")
 
 # Read dataset and split into training and test data
 data = pd.read_csv("wine_dataset.csv")
@@ -23,21 +26,29 @@ sklearn_model, sklearn_params = eval.best_model_of_sklearn()
 
 
 # Train and test each model
+
+impl_start = time()
 implementation_model.learn(
     X_train, y_train, implementation_params[0], implementation_params[1]
 )
 prediction = implementation_model.predict(x_test)
+impl_end = time()
+
 score = accuracy_score(y_test, prediction)
 
-
+sk_start = time()
 sklearn_model.fit(X_train, y_train)
 sklearn_prediction = sklearn_model.predict(x_test)
+sk_end = time()
+
 sklearn_score = accuracy_score(y_test, sklearn_prediction)
 
 # Results
 print(f"\nBest implemented model with parameters: {implementation_params}.")
 print(f"accuracy: {score}")
+print(f"Time training and testing: {impl_end-impl_start}")
 
 
 print(f"\nBest sklearn model with parameters: {sklearn_params}")
 print(f"accuracy: {sklearn_score}")
+print(f"Time training and testing: {sk_end-sk_start}")
